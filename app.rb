ed2aaa7d8539b44c8b7d7f3d4482fd71e9e83865
baileyshelden <bailey7748@gmail.com>
2018-06-12 17:45:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "bailey ig "
+  end 
+   get '/hometown' do 
+     "HONESTLY WISH it was hawaii tbh"
+   end 
+  get '/favorite_song' do 
+    "ANYTHING BY ED SHEERAN BABBBBY"
+  end 
 end
